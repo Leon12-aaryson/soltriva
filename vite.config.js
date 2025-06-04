050ab177f462530fe386cbf3bc4b3d50ff9d6d96
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2025-06-04 21:34:07 +0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: '/build/',
     // base: process.env.ASSET_URL + "/",
     plugins: [
         laravel({
